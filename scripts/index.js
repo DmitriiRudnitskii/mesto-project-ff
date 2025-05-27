c9bb1fe62749ce4bfc57129f60f8754f1d0001a1
Dmitrii Rudnitskii <dimarud1996@gmail.com>
2025-05-27 19:19:47 +0300

    Step 2

--- scripts/index.js
+++ scripts/index.js
@@ -8,19 +8,26 @@
 
 // @todo: Вывести карточки на страницу
 
-const cardsTemplate = document.querySelector('#card-template').content;
+const cardsTemplate = document.querySelector("#card-template").content;
 function createCard(cardObject, deleteCallback) {
   const cardElement = cardsTemplate.cloneNode(true);
-  cardElement.querySelector('.card__title').textContent = cardObject.name;
-  cardElement.querySelector('.card__image').src = cardObject.link;
-  cardElement.querySelector('.card__image').alt = cardObject.name;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
-    deleteCallback();
-  });
+  cardElement.querySelector(".card__title").textContent = cardObject.name;
+  cardElement.querySelector(".card__image").src = cardObject.link;
+  cardElement.querySelector(".card__image").alt = cardObject.name;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", (evt) => {
+      const deleteButton = evt.target.closest('.card');
+      deleteCallback(deleteButton);
+    });
 
   return cardElement;
-  
 }
 
-initialCards.forEach(card => document.querySelector('.places__list').append(createCard(card, () => {
-})));
+initialCards.forEach((card) =>
+  document.querySelector(".places__list").append(createCard(card, deleteCard)),
+);
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
